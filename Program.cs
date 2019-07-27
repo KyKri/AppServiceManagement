c5612fe1a62e321de7608f928957ed5a14ea3c14
Kyle Krick <krkrick@gmail.com>
2019-07-26 23:28:35 -0700

    added using statements

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.AppService.Fluent.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Azure.Management.Samples.Common;
+using System.IO;
+using System.Net.Http;
 
 namespace AppServiceManagement
 {
