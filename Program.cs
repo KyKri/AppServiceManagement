df03b82b7d7a204b8c2ce1890018df4ca4b02c20
Kyle Krick <krkrick@gmail.com>
2019-07-28 22:51:24 -0700

    established connection to azure subscription

--- Program.cs
+++ Program.cs
@@ -4,9 +4,12 @@ using Microsoft.Azure.Management.AppService.Fluent.Models;
 using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
-using Microsoft.Azure.Management.Samples.Common;
+// using Microsoft.Azure.Management.Samples.Common;
 using System.IO;
 using System.Net.Http;
+
+// App service management example 
+// https://github.com/Azure-Samples/app-service-dotnet-manage-web-apps-with-custom-domains/blob/master/Program.cs
 
 namespace AppServiceManagement
 {
@@ -14,7 +17,18 @@ namespace AppServiceManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var creds = SdkContext.AzureCredentialsFactory.FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
+
+                var azure = Azure.Configure().WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic).Authenticate(creds).WithDefaultSubscription();
+
+                Console.WriteLine($"Selected subscription is: {azure.SubscriptionId}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
